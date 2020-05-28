573495165a44366620e389f0d6b4a29e41f7a0e9
Camilo Rivera <damelines@gmail.com>
2020-05-28 20:10:26 +1000

    day 1 - further simplification

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -1,21 +1,13 @@
 (ns day1.core
   (:require [clojure.string :as str]))
 
-(defn parse-freq
-  [freq]
-  (Integer/parseInt freq))
-
 (defn split-input
   [input]
   (into [] (map str/trim (str/split input #","))))
-
-(defn as-numbers 
-  [strv]
-  (map parse-freq strv))
 
 (defn final-freq
   [input]
   (->> input
     (split-input)
-    (as-numbers)
+    (map #(Integer/parseInt %))
     (reduce +)))
